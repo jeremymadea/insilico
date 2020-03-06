e0470185e66dbcc52940405886a2bbeae03b7978
Jeremy Madea <jdmadea@gmail.com>
2020-03-06 15:22:07 -0500

    Adds explorer mode.

---
+++ html.go
@@ -0,0 +1,193 @@
+package main
+
+var rootHtml = `
+{{define "td" -}}
+<td><a href="/?rd={{.}}"><img src="/capng?rd={{.}}&w=100&h=100" /></a></td>
+{{- end}}
+<html>
+<head>
+<title>InSilico Explorer</title>
+<style type="text/css">
+ body {
+   font-family: sans-serif; 
+   font-size: 10pt;
+ }
+ table { 
+   margin-left: auto; 
+   margin-right: auto;
+ }
+ img, table, td, tr {
+   padding: 0px;
+   border: 0px;
+   border-spacing: 0px;
+ }
+ #shutdown-link {
+   position: absolute;
+   bottom: 5px;
+   right: 5px;
+ }
+
+</style>
+<body>
+<div style="text-align: center;">
+<h1>Ruleset: {{.C.Ruleset}}</h1>
+</div>
+<table>
+<tr>
+{{range .MutationSetA}}{{template "td" .}}{{end}}
+</tr><tr>
+{{range .MutationSetB}}{{template "td" .}}{{end}}
+<td colspan="4" rowspan="4">
+  <a href="/scope?rd={{.C.Ruleset}}&w=640&h=640"><img src="/capng?rd={{.C.Ruleset}}" /></a>
+</td>
+{{range .MutationSetC}}{{template "td" .}}{{end}}
+</tr><tr>
+{{range .MutationSetD}}{{template "td" .}}{{end}}
+</tr><tr>
+{{range .MutationSetE}}{{template "td" .}}{{end}}
+<td>
+</tr><tr>
+{{range .MutationSetF}}{{template "td" .}}{{end}}
+</tr><tr>
+{{range .MutationSetG}}{{template "td" .}}{{end}}
+</tr>
+</table>
+<a id="shutdown-link" href="/shutdown">Shutdown InSilico Explorer</a>
+</body>
+</html>
+`
+
+var scopeHtml = `<html>
+<head>
+ <title>Scope - Ruleset: {{.C.RSHex}}</title>
+ <style type="text/css">
+ body { 
+   font-family: sans-serif; 
+   font-size: 10pt;
+ }
+ .form-fieldset {
+   display: table;
+ }
+
+ .form-field {
+   display: table-row;
+ }
+
+ .field-label,
+ .form-field-input,
+ .form-field-comment {
+   display: table-cell;
+   padding: 3px 10px;
+ }
+
+ #chooser-link { 
+   position: absolute;
+   top: 5px; 
+   right: 5px;
+ }
+
+ #shutdown-link {
+   position: absolute;
+   bottom: 5px;
+   right: 5px;
+ }
+
+ #scope-image {
+   float: left;
+   padding-right: 10px;
+ }
+ </style>
+</head>
+<body>
+
+<div id="scope-image">
+<a href=""><img src="/capng{{.QueryString}}"></a>
+</div>
+
+<div id="scope-right">
+<h1>RuleSet: {{.C.RSHex}}</h1>
+<hr>
+<div id="scope-form">
+ <form action="" method=get>
+ <input type="hidden" name="rd" value="{{.C.Ruleset}}">
+  <fieldset><legend>Dimensions</legend>
+  <div class="form-fieldset">
+   <div class="form-field">
+    <label class="field-label" for="w">Width:</label>
+    <div class="form-field-input">
+     <input id="w" type="text" size="4" name="w" value="{{.C.Width}}" />
+    </div>
+    <div class="form-field-comment">The width of the image.</div>
+   </div>
+   <div class="form-field">
+    <label class="field-label" for="h">Height:</label>
+    <div class="form-field-input">
+     <input id="h" type="text" size="4" name="h" value="{{.C.Height}}" />
+    </div>
+    <div class="form-field-comment">The height of the image.</div>
+   </div>
+  </div>
+  </fieldset>
+
+  <fieldset><legend>Cell Colors</legend>
+  <div class="form-fieldset">
+   <div class="form-field">
+    <label class="field-label" for="dc">Dead Cell Color:</label>
+    <div class="form-field-input">
+     <input id="dc" name="dc" type="color" value="#{{.C.DColorHex}}" />
+    </div>
+    <div class="form-field-comment"></div>
+   </div>
+   <div class="form-field">
+    <label class="field-label" for="lc">Live Cell Color:</label>
+    <div class="form-field-input">
+     <input id="lc" name="lc" type="color" value="#{{.C.LColorHex}}" />
+    </div>
+    <div class="form-field-comment"></div>
+   </div>
+  </div>
+  </fieldset>
+
+  <fieldset><legend>Initialization Parameters</legend>
+  <div class="form-fieldset">
+
+  <div class="form-field">
+   <label class="field-label" for="p">Initial Live %:</label>
+   <div class="form-input">
+     <input type="text" size="4" name="p" value="{{.C.Percentage}}" />
+   </div>
+   <div class="form-field-comment">Random mode only.</div>
+  </div>
+
+  <div class="form-field">
+   <label class="field-label" for="m">Initialization Mode:</label>
+   <div class="form-input">
+     <select id="mode-drop-down" name="m">
+{{.ModeOpts}}
+     </select>
+     <!-- <input type="text" size="6" name="m" value="{{.C.InitMode}}" /> -->
+   </div> 
+   <div class="form-field-comment">center|ctralt|random|repeat|live|dead</div> 
+  </div>  
+
+  <div class="form-field">
+   <label class="field-label" for="s">Pattern String:</label>
+   <div class="form-input">
+    <input type="text" size="20" name="s" value="{{.C.Pattern}}">
+   </div>
+   <div class="form-field-comment"></div>
+  </div>
+  </div>
+  </fieldset>
+  <br />
+  <input type="submit">
+ </form>
+</div><!-- scope-form -->
+<a id="chooser-link" href="/?rd={{.C.Ruleset}}">Back to Chooser</a>
+</div>
+<div id="footer">
+<a id="shutdown-link" href="/shutdown">Shutdown InSilico Explorer</a>
+</div>
+</body>
+</html>
+`
--- main.go
+++ main.go
@@ -4,71 +4,227 @@
 package main
 
 import (
-        "flag"
+	"flag"
 	"fmt"
+	"html/template"
 	"image"
 	"image/color"
 	"image/png"
 	"log"
 	"math/rand"
+	"net/http"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"time"
-        "github.com/jeremymadea/insilico/ca1d"
+	"github.com/jeremymadea/insilico/ca1d"
 )
 
-const defWidth, defHeight = 640, 640
+const defWidth, defHeight = 400, 400
 
-var liveColor = color.NRGBA{R:255, G:255, B:255, A:255};
-var deadColor = color.NRGBA{R:0,   G:0,   B:0,   A:255};
+type Config struct {
+	Width, Height int
+	Pattern string
+	InitMode string
+	Seed int64
+	Percentage float64
+	Ruleset int
+	RSHex string
+	LColorHex string
+	LiveColor color.Color
+	DColorHex string
+	DeadColor color.Color
+}
+
+var global Config
+var gimg *image.NRGBA
+
+func openbrowser(url string) {
+        var err error
+
+		err = exec.Command("open", url).Start()
+        if err != nil {
+                log.Fatal(err)
+        }
+
+}
+
+func querystring(c Config) string {
+	return "?" +
+		"rd=" + fmt.Sprintf("%d", c.Ruleset) +
+		"&w=" + fmt.Sprintf("%d", c.Width) +
+		"&h=" + fmt.Sprintf("%d", c.Height) +
+		"&p=" + fmt.Sprintf("%.2f", c.Percentage) +
+		"&m=" + c.InitMode +
+		"&s=" + c.Pattern +
+		"&lc=" + c.LColorHex +
+		"&dc=" + c.DColorHex
+
+}
+
+func html_modeopts(curmode string) template.HTML {
+	modes := []string{"random", "center", "ctralt", "repeat", "live", "dead"}
+    s := ""
+	selected := ""
+    for _, m := range modes {
+		if m == curmode {
+			selected = " selected"
+		} else {
+			selected = ""
+		}
+		s += fmt.Sprintf(`<option value="%v"%v>%v</option>`, m, selected, m)
+        s += "\n"
+    }
+	return template.HTML(s)
+}
 
 func hex2color(hex string) color.NRGBA {
 	rgb := make([]uint8,4)
 	for i:=0; i<len(hex) && i<6; i+=2 {
-		if u, e := strconv.ParseUint(hex[i:i+2], 16, 8); e == nil { 
+		if u, e := strconv.ParseUint(hex[i:i+2], 16, 8); e == nil {
 			rgb[i/2] = uint8(u)
-		} else { 
+		} else {
 			rgb[i/2] = 0
 		}
 	}
 	return color.NRGBA{R:rgb[0], G:rgb[1], B:rgb[2], A:255}
 }
 
-func MakeImage(ca *ca1d.CA1D, h int, w, b color.Color) *image.NRGBA {
-        img := image.NewNRGBA(image.Rect(0, 0, ca.Width, h))
+func mutations(ruleset int) []int {
+    ret := make([]int, 32)
+    for i:=0; i<32; i++ {
+		ret[i] = ruleset ^ (1 << i)
+    }
+	return ret
+}
 
-        // We create one horizontal line of pixels per generation of
-        // the CA.
-        for y := 0; y < h; y++ {
-                for x := 0; x < ca.Width; x++ {
-                        if ca.Current[x] == ca1d.Live {
-                                img.Set(x, y, w)
-                        } else {
-                                img.Set(x, y, b)
-                        }
-                }
-                ca.Generate() // Calculate the next generation of the CA.
-        }
+func MakeImage(ca *ca1d.CA1D, h int, w, b color.Color) *image.NRGBA {
+	img := image.NewNRGBA(image.Rect(0, 0, ca.Width, h))
+
+	// We create one horizontal line of pixels per generation of
+	// the CA.
+	for y := 0; y < h; y++ {
+		for x := 0; x < ca.Width; x++ {
+			if ca.Current[x] == ca1d.Live {
+				img.Set(x, y, w)
+			} else {
+				img.Set(x, y, b)
+			}
+		}
+		ca.Generate() // Calculate the next generation of the CA.
+	}
 	return img
 }
 
+func ExecuteCA(cfg Config) *image.NRGBA {
+	ca := ca1d.NewCA1D(cfg.Width, uint32(cfg.Ruleset))
+	switch cfg.InitMode {
+		case "random":
+			ca.InitRandom(cfg.Percentage)
+		case "center":
+			ca.InitCenter(ca1d.Dead, cfg.Pattern)
+		case "ctralt":
+			ca.InitCenter(ca1d.Live, cfg.Pattern)
+		case "repeat":
+			ca.InitRepeat(cfg.Pattern)
+		case "live":
+			ca.InitSimple(ca1d.Live)
+		case "dead":
+			ca.InitSimple(ca1d.Dead)
+		default:
+			cfg.InitMode = "random"
+			ca.InitRandom(cfg.Percentage)
+	}
+	return MakeImage(ca, cfg.Height, cfg.LiveColor, cfg.DeadColor)
+}
 
+func NewConfigFromForm(r *http.Request) (cfg Config) {
+	cfg = global
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+    for k := range r.Form {
+		v := r.Form.Get(k)
+		switch k {
+			case "rd":
+				if iv, err := strconv.Atoi(v); err == nil {
+					cfg.Ruleset = iv
+				}
+				if cfg.Ruleset < 0 {
+					cfg.Ruleset = int(rand.Uint32())
+				}
+			case "rh":
+				if uv, e := strconv.ParseUint(v, 16, 32); e == nil {
+					cfg.Ruleset = int(uv)
+				}
+			case "s":
+				cfg.Pattern = v
+			case "p":
+				if fv, err := strconv.ParseFloat(v, 64); err == nil {
+					cfg.Percentage = fv
+				}
+			case "m":
+				cfg.InitMode = v
+				switch cfg.InitMode {
+					case "random":
+					case "center":
+					case "ctralt":
+					case "repeat":
+					case "live":
+					case "dead":
+					default: cfg.InitMode = "random"
+				}
+			case "w":
+				if iv, err := strconv.Atoi(v); err == nil {
+					if iv > 0 {
+						cfg.Width = iv
+					}
+				}
+			case "h":
+				if iv, err := strconv.Atoi(v); err == nil {
+					if iv > 0 {
+						cfg.Height = iv
+					}
+				}
+			case "dc":
+                if v[0] == '#' && len(v) == 7 {
+					v = v[1:]
+				}
+				cfg.DColorHex = v
+			case "lc":
+                if v[0] == '#' && len(v) == 7 {
+					v = v[1:]
+				}
+				cfg.LColorHex = v
+			default:
+				log.Print("Bad Form Key: ", k)
+		}
+    }
+	cfg.RSHex = fmt.Sprintf("%08X", cfg.Ruleset)
+	cfg.DeadColor = hex2color(cfg.DColorHex)
+	cfg.LiveColor = hex2color(cfg.LColorHex)
+	return cfg
+}
 
 func main() {
 	defaultSeed := time.Now().UnixNano()
- 
+
 	// Setup and parse command line arguments.
+	explorerMode := false
+	flag.BoolVar(&explorerMode, "explorer", explorerMode,
+		"Start in explorer mode. Runs a webserver on localhost:8084")
+
 	startPattern := "1"
-	flag.StringVar(&startPattern, "s", startPattern, 
+	flag.StringVar(&startPattern, "s", startPattern,
 		"Set start pattern.")
 
 	initMode := "random"
-	flag.StringVar(&initMode, "m", initMode, 
+	flag.StringVar(&initMode, "m", initMode,
 		"Set init mode. random|center|ctralt|repeat|live|dead")
-    
+
 	seed := int64(-1)
-	flag.Int64Var(&seed, "seed", seed, 
+	flag.Int64Var(&seed, "seed", seed,
 		"Set the seed. Use the system time if negative.")
 
 	width, height := defWidth, defHeight
@@ -79,15 +235,15 @@ func main() {
 	flag.Float64Var(&pct, "p", pct, "Set random init `percentage`.")
 
 	ruleset := -1
-	flag.IntVar(&ruleset, "r", ruleset, 
+	flag.IntVar(&ruleset, "r", ruleset,
 		"Set ruleset. Choose random one if negative.")
 
 	rshex := ""
-	flag.StringVar(&rshex, "R", rshex, 
+	flag.StringVar(&rshex, "R", rshex,
 		"Set ruleset. (8 digit hex) Note: -r will be ignored.")
 
 	outfn := "image.png"
-	flag.StringVar(&outfn, "o", outfn, 
+	flag.StringVar(&outfn, "o", outfn,
 		"Set the output file.")
 
 	white := "FFFFFF" // white defaults to white. 
@@ -98,63 +254,157 @@ func main() {
 
 	flag.Parse()
 
-	liveColor = hex2color(white) // white might not be white.
-	deadColor = hex2color(black) // black might not be black.
+	liveColor := hex2color(white) // white might not be white.
+	deadColor := hex2color(black) // black might not be black.
+
+	// Copy vars into global Config struct. TODO: remove the intermediate vars
+	global.LColorHex = white
+	global.LiveColor = liveColor
+	global.DColorHex = black
+	global.DeadColor = deadColor
+
+	global.Width = width
+	global.Height = height
+	global.Percentage = pct
+	global.InitMode = initMode
+	global.Pattern = startPattern
 
 	// A negative value for seed means we must use the default.
-	if seed < 0 { 
+	if seed < 0 {
 		rand.Seed(defaultSeed)
+		global.Seed = defaultSeed
 	} else {
 		rand.Seed(seed)
+		global.Seed = seed
 	}
 
 	// A negative value for ruleset means we must choose a random one.
-	if ruleset < 0 { 
+	if ruleset < 0 {
 		ruleset = int(rand.Uint32())
 	}
-
 	if rshex != "" {
-		if u, e := strconv.ParseUint(rshex, 16, 32); e == nil { 
+		if u, e := strconv.ParseUint(rshex, 16, 32); e == nil {
 			ruleset = int(u)
 		}
 	}
 
-	ca := ca1d.NewCA1D(width, uint32(ruleset))
+	global.Ruleset = ruleset
+    global.RSHex = fmt.Sprintf("%08X", ruleset)
+	gimg = ExecuteCA(global)
 
-	switch initMode { 
-		case "random":
-			ca.InitRandom(pct)
-		case "center":
-			ca.InitCenter(ca1d.Dead, startPattern)
-		case "ctralt":
-			ca.InitCenter(ca1d.Live, startPattern)
-		case "repeat":
-			ca.InitRepeat(startPattern)
-		case "live":
-			ca.InitSimple(ca1d.Live)
-		case "dead":
-			ca.InitSimple(ca1d.Dead)
-		default:
-			initMode = "random"
-			ca.InitRandom(pct)
+	if explorerMode {
+		http.HandleFunc("/", webRoot)
+		http.HandleFunc("/debug", webDebug)
+		http.HandleFunc("/scope", webScope)
+		http.HandleFunc("/capng", webCapng)
+		http.HandleFunc("/shutdown", webShutdown)
+		go openbrowser("http://localhost:8084")
+		log.Fatal(http.ListenAndServe("localhost:8084", nil))
+
+	} else {
+		// Running as a command line tool. Just write our image out.
+		outfn = strings.Replace(outfn, "$", fmt.Sprintf("%08X",ruleset), 1)
+		outfn = strings.Replace(outfn, "#", fmt.Sprintf("%d",ruleset), 1)
+
+		f, err := os.Create(outfn)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := png.Encode(f, gimg); err != nil {
+			f.Close()
+			log.Fatal(err)
+		}
+
+		if err := f.Close(); err != nil {
+				log.Fatal(err)
+		}
+	}
+}
+
+// Following are handlers for web requests when we are running in explorer mode
+
+func webDebug(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s %s %s\n", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
+	fmt.Fprintf(w, "Host = %q\n", r.Host)
+	fmt.Fprintf(w, "RemoteAddr = %q\n", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		log.Print(err)
+	}
+	for k, v := range r.Form {
+		fmt.Fprintf(w, "Form[%q] = %q\n", k, v)
+	}
+}
+
+func webRoot(w http.ResponseWriter, r *http.Request) {
+	cfg := NewConfigFromForm(r);
+	t := template.Must(template.New("root").Parse(rootHtml))
+	muts := mutations(global.Ruleset)
+
+	data := struct{
+		C Config
+		MutationSetA []int
+		MutationSetB []int
+		MutationSetC []int
+		MutationSetD []int
+		MutationSetE []int
+		MutationSetF []int
+		MutationSetG []int
+	}{
+		cfg,
+		muts[:8],
+		muts[8:10],
+		muts[10:12],
+		muts[12:16],
+		muts[16:20],
+		muts[20:24],
+		muts[24:],
+    }
+    err := t.Execute(w, data)
+	if (err != nil) {
+		log.Print(err)
+	}
+}
+
+func webScope(w http.ResponseWriter, r *http.Request) {
+	cfg := NewConfigFromForm(r);
+    qs := querystring(cfg)
+    modeopts := html_modeopts(cfg.InitMode)
+
+	if r.URL.RawQuery == "" {
+		cfg.Width = 640;
+		cfg.Height = 640;
 	}
 
-	img := MakeImage(ca, height, liveColor, deadColor)
+	t := template.Must(template.New("scope").Parse(scopeHtml))
 
-        outfn = strings.Replace(outfn, "$", fmt.Sprintf("%08X",ruleset), 1)
-        outfn = strings.Replace(outfn, "#", fmt.Sprintf("%d",ruleset), 1)
-
-	f, err := os.Create(outfn)
-	if err != nil {
-		log.Fatal(err)
+	data := struct {
+		C Config
+		QueryString string
+		ModeOpts template.HTML
+	}{
+		cfg,
+		qs,
+		modeopts,
 	}
 
-	if err := png.Encode(f, img); err != nil {
-		f.Close()
-		log.Fatal(err)
+    err := t.Execute(w, data)
+	if (err != nil) {
+		log.Print(err)
 	}
+}
 
-	if err := f.Close(); err != nil {
-		log.Fatal(err)
-	}
+func webCapng(w http.ResponseWriter, r *http.Request) {
+	cfg := NewConfigFromForm(r);
+	img := ExecuteCA(cfg)
+
+	png.Encode(w, img)
+}
+
+func webShutdown(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Shutting down InSilico Explorer.")
+	os.Exit(0)
 }
